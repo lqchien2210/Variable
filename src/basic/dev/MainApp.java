ef362ae067a671ca1158ab38fb6636a0d2c5a8b5
lqchien2210 <82997980+lqchien2210@users.noreply.github.com>
2021-04-22 14:49:23 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+/**
+*
+* @author TranThanhBinh
+*/
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		* Khai báo biến
+		*/
+		String firstName = "Le Quy";
+		String lastName = "Chien";
+		int old = 18;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va Ten: ");
+		System.out.println(firstName +" "+lastName);
+		System.out.println("Mark Math: "+markMath);
+		System.out.println("Passed: "+passed);
+		
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+	}
+
+}
